1e4eb751cf12b1469bb582f29121fa7ea5bcea87
Artur <nasibullin.artur@gmail.com>
2022-07-31 04:35:40 +0500

    final commit

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const AppHeader = ({ liked, allPosts }) => {
 	return (
 		<div className='app-header d-flex'>
-			<h1>Artur Nasibullin Social Club</h1>
+			<h1>Artur Nasibullin Bad Twitter</h1>
 			<h2>
 				{allPosts} записей, из них понравилось {liked}
 			</h2>
